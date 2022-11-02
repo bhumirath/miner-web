278d1ced333978d8d3e8af65ed1406ddbbf08a08
siwakornpatsalao <siwakorn.pat@ku.th>
2022-11-02 19:12:01 +0700

    add Inbox

---
+++ src/main/java/th/ac/ku/restaurant/controller/InboxController.java
@@ -0,0 +1,21 @@
+package th.ac.ku.restaurant.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import th.ac.ku.restaurant.service.MenuService;
+
+@Controller
+public class InboxController {
+    @Autowired
+    private MenuService service;
+
+
+    @GetMapping("/inbox")
+    public String getInboxPage(Model model) {
+        model.addAttribute("menus", service.getMenus());
+        return "inbox";
+    }
+
+}
