7690634569d0c95b5618e2eec4e79f4758533763
siwakornpatsalao <siwakorn.pat@ku.th>
2022-11-07 02:06:52 +0700

    add date

--- src/main/java/th/ac/ku/restaurant/controller/DelegateController.java
+++ src/main/java/th/ac/ku/restaurant/controller/DelegateController.java
@@ -1,6 +1,7 @@
 package th.ac.ku.restaurant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.util.DateUtils;
 import th.ac.ku.restaurant.model.Order;
 import th.ac.ku.restaurant.model.WorkOrder;
 import th.ac.ku.restaurant.repository.OrderRepository;
@@ -17,6 +19,8 @@ import th.ac.ku.restaurant.service.SignupService;
 import th.ac.ku.restaurant.service.WorkOrderService;
 
 import javax.validation.Valid;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.UUID;
 
 @Controller
@@ -50,7 +54,25 @@ public class DelegateController {
     public String delegateOrder(Model model, @Valid @ModelAttribute("newWorkOrder") WorkOrder workOrder, BindingResult result) {
         if (result.hasErrors())
             return "delegate";
-        //เก็บworkOrder
+        
+        Date startDate = workOrder.getStartDate();
+        Date finishDate = workOrder.getFinishDate();
+
+        Calendar c = Calendar.getInstance();
+        Calendar c2 = Calendar.getInstance();
+
+        c.setTime(startDate);
+        c2.setTime(finishDate);
+
+        c.add(Calendar.YEAR, 543);
+
+        c2.add(Calendar.YEAR, 543);
+
+        Date startDate2 = c.getTime();
+        Date finishDate2 = c2.getTime();
+        workOrder.setStartDate(startDate2);
+        workOrder.setFinishDate(finishDate2);
+
         workOrder.setWorkStatus("Not finish");
         workOrderService.create(workOrder);
         return "redirect:/inbox"; //กลับหน้า inbox
